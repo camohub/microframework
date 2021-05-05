62c30d8bb1ee40cfaee25c99f55f409d7b28f93a
Čamaj Vladimír <vladimir.camaj@gmail.com>
2021-05-05 22:14:27 +0200

    Application.php upper case fix

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-require_once(__DIR__ . '/framework/application.php');
+require_once(__DIR__ . '/framework/Application.php');
 
 $application = new Application();
 $application->run();
